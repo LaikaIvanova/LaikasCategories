4088764da93005ca5255a85d4e8fd6379dc48197
Laika Ivanova <laika.antonovna.ivanova@gmail.com>
2025-09-26 17:40:59 +0200

    Added Base.BookCarving4

--- common/media/lua/client/LICL_Item_Categories.lua
+++ common/media/lua/client/LICL_Item_Categories.lua
@@ -1563,6 +1563,7 @@ TweakItem("Base.BookHusbandry5","DisplayCategory","Literatur_[Lehrbuch]");
 TweakItem("Base.BookCarving1","DisplayCategory","Literatur_[Lehrbuch]");
 TweakItem("Base.BookCarving2","DisplayCategory","Literatur_[Lehrbuch]");
 TweakItem("Base.BookCarving3","DisplayCategory","Literatur_[Lehrbuch]");
+TweakItem("Base.BookCarving4","DisplayCategory","Literatur_[Lehrbuch]");
 -- Recipe magazine
 TweakItem("Base.FishingMag1","DisplayCategory","Literatur_[Magazin]");
 TweakItem("Base.FishingMag2","DisplayCategory","Literatur_[Magazin]");
